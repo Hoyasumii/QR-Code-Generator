f51fbf1f17d0495e866570fc42c890a1efa8622d
Alan Reis <alanreisanjo@gmail.com>
2025-05-12 21:21:49 -0300

    Ajustes finais?

--- src/services/get-location.ts
+++ src/services/get-location.ts
@@ -1,7 +1,9 @@
 import type { LocationInterface } from "./types";
 
 export async function getLocation(): Promise<LocationInterface> {
-  const locationRequest = await fetch(process.env.LOCATION_API_URL);
+  const locationRequest = await fetch(process.env.LOCATION_API_URL, {
+    cache: "force-cache",
+  });
 
   if (!locationRequest.ok) throw new Error();
 
